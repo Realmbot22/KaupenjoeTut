03436a733893b41a6afa4b62aabbe73dfded8173
Realmbot22 <go.geckos.412@gmail.com>
2025-03-31 22:07:59 -0400

    Added pink garnet

--- src/main/java/net/genesis/tutorialmod/DomsMod.java
+++ src/main/java/net/genesis/tutorialmod/DomsMod.java
@@ -2,6 +2,7 @@ package net.genesis.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.genesis.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +13,6 @@ public class DomsMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
