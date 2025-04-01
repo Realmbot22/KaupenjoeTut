1767528fa28a49352c51b8cdab08ea9703635baf
Realmbot22 <go.geckos.412@gmail.com>
2025-03-31 21:33:57 -0400

    Initial commit

---
+++ src/main/java/net/genesis/tutorialmod/DomsModClient.java
@@ -0,0 +1,10 @@
+package net.genesis.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DomsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/genesis/tutorialmod/DomsModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.genesis.tutorialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DomsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
